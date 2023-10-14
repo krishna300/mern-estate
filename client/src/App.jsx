c8dae5a22a188bfaaa3834a308610522307ef513
Krishna Kondapalli <krishna.kondapalli@loqbox.com>
2023-10-14 10:10:30 +0530

    Created Router and configured Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import Home from './components/Home'
+import Signin from './components/Signin'
+import Signup from './components/Signup'
+import About from './components/About'
+import Profile from './components/Profile'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
     <>
       <div>Hello Client</div>
-       
+       <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/sign-in' element={<Signin/>}/>
+          <Route path='/sign-up' element={<Signup/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+
+        </Routes>
+       </BrowserRouter>
     </>
   )
 }
